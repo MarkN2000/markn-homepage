b74eaeb450ad1df47a7b28144f4c5ff7051048b3
markn <akaharaimori2000@gmail.com>
2025-05-16 19:39:12 +0900

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,16 @@ module.exports = {
     './assets/js/**/*.js', // JavaScriptファイル (Alpine.jsなどを使用する場合)
     // 必要に応じて他のパスを追加
   ],
-  theme: {
+   theme: {
     extend: {
-      // ここにテーマのカスタマイズを記述します
-      // 例:
-      // colors: {
-      //   'brand-blue': '#1992d4',
-      // },
-      // fontFamily: {
-      //   sans: ['Inter', 'sans-serif'],
-      // },
+      colors: {
+        'custom-light-blue': '#ADD8E6', // 例: 明るい水色 (具体的なカラーコードは調整可能)
+        'custom-dark-blue': '#0077B6',  // 例: やや濃い水色 (リンク色など)
+        // 他にも名刺で使われている色があれば追加
+      },
+      backgroundImage: {
+        'tartan-check': "url('/images/bg.png')",
+      }
     },
   },
   plugins: [
