a0f5aee5dde3c7d2464fd3ff02a0ba380445aeb5
markn <akaharaimori2000@gmail.com>
2025-05-17 15:12:09 +0900

    リセット

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
-// postcss.config.js
 module.exports = {
-    plugins: {
-        'tailwindcss': {}, // Tailwind CSS をPostCSSプラグインとして使用
-        'autoprefixer': {}, // 必要に応じて autoprefixer も追加
-        // 必要に応じて他のPostCSSプラグイン
-    }
-}
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+};
