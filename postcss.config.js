36b76dfed3bd5f2f2f9e1998f488e3019d97b779
markn <akaharaimori2000@gmail.com>
2025-05-17 13:45:45 +0900

    Tailwind CSS v4対応

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// postcss.config.js
+module.exports = {
+    plugins: {
+        'tailwindcss': {}, // Tailwind CSS をPostCSSプラグインとして使用
+        'autoprefixer': {}, // 必要に応じて autoprefixer も追加
+        // 必要に応じて他のPostCSSプラグイン
+    }
+}
